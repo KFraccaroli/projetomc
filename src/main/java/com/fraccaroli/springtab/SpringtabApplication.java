fa0be32c7edad38f19db43e2532fcde017a6a8e4
Kaue Fraccaroli <kaue_fraccaroli@yahoo.com.br>
2018-05-23 20:09:26 -0300

    Projeto criado

---
+++ src/main/java/com/fraccaroli/springtab/SpringtabApplication.java
@@ -0,0 +1,12 @@
+package com.fraccaroli.springtab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtabApplication.class, args);
+	}
+}
