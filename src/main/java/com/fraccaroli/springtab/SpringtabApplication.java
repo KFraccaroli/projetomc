9e9dc16093f274da43e3617908297970bf213a95
Kaue Fraccaroli <kaue_fraccaroli@yahoo.com.br>
2018-05-23 23:26:38 -0300

    Criando operacao de instanciacao

--- src/main/java/com/fraccaroli/springtab/SpringtabApplication.java
+++ src/main/java/com/fraccaroli/springtab/SpringtabApplication.java
@@ -1,12 +1,32 @@
 package com.fraccaroli.springtab;
 
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringtabApplication {
+import com.fraccaroli.springtab.domain.Categoria;
+import com.fraccaroli.springtab.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class SpringtabApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringtabApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 }
