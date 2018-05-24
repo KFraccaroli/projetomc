3969eea9c647015d4c565351a718483606af0b29
Kaue Fraccaroli <kaue_fraccaroli@yahoo.com.br>
2018-05-23 21:07:53 -0300

    Criando repository e service para para Categoria

---
+++ src/main/java/com/fraccaroli/springtab/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.fraccaroli.springtab.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fraccaroli.springtab.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
