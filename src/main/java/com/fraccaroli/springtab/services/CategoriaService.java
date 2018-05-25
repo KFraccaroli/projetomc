ab488fbdb45279dda575c5b65cc0f00d5764c20b
Kaue Fraccaroli <kaue_fraccaroli@yahoo.com.br>
2018-05-24 21:38:16 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/fraccaroli/springtab/services/CategoriaService.java
+++ src/main/java/com/fraccaroli/springtab/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.fraccaroli.springtab.domain.Categoria;
 import com.fraccaroli.springtab.repositories.CategoriaRepository;
+import com.fraccaroli.springtab.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,8 +15,9 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria buscar(Integer id){
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+					"Objeto não encontrato! id: " +  id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
