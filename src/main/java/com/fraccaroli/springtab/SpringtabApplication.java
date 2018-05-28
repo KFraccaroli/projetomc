08f59d1ccf608b9d12a44f8fce78eb81b63ea26f
Kaue Fraccaroli <kaue_fraccaroli@yahoo.com.br>
2018-05-28 20:41:50 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/fraccaroli/springtab/SpringtabApplication.java
+++ src/main/java/com/fraccaroli/springtab/SpringtabApplication.java
@@ -1,6 +1,7 @@
 package com.fraccaroli.springtab;
 
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +14,20 @@ import com.fraccaroli.springtab.domain.Cidade;
 import com.fraccaroli.springtab.domain.Cliente;
 import com.fraccaroli.springtab.domain.Endereco;
 import com.fraccaroli.springtab.domain.Estado;
+import com.fraccaroli.springtab.domain.Pagamento;
+import com.fraccaroli.springtab.domain.PagamentoComBoleto;
+import com.fraccaroli.springtab.domain.PagamentoComCartao;
+import com.fraccaroli.springtab.domain.Pedido;
 import com.fraccaroli.springtab.domain.Produto;
+import com.fraccaroli.springtab.domain.enums.EstadoPagamento;
 import com.fraccaroli.springtab.domain.enums.TipoCliente;
 import com.fraccaroli.springtab.repositories.CategoriaRepository;
 import com.fraccaroli.springtab.repositories.CidadeRepository;
 import com.fraccaroli.springtab.repositories.ClienteRepository;
 import com.fraccaroli.springtab.repositories.EnderecoRepository;
 import com.fraccaroli.springtab.repositories.EstadoRepository;
+import com.fraccaroli.springtab.repositories.PagamentoRepository;
+import com.fraccaroli.springtab.repositories.PedidoRepository;
 import com.fraccaroli.springtab.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,7 +44,11 @@ public class SpringtabApplication implements CommandLineRunner{
 	@Autowired
 	private ClienteRepository clienteRepository;
 	@Autowired
-	private EnderecoRepository enderecoRepository;	
+	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringtabApplication.class, args);
@@ -83,5 +95,20 @@ public class SpringtabApplication implements CommandLineRunner{
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/07/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
 	}
 }
---
+++ src/main/java/com/fraccaroli/springtab/repositories/PedidoRepository.java
@@ -0,0 +1,11 @@
+package com.fraccaroli.springtab.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fraccaroli.springtab.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
+
+}
