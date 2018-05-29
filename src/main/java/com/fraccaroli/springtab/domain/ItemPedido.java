6e0a45725021b717d21694c161779d0a3f2d334f
Kaue Fraccaroli <kaue_fraccaroli@yahoo.com.br>
2018-05-28 22:55:55 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/fraccaroli/springtab/domain/ItemPedido.java
+++ src/main/java/com/fraccaroli/springtab/domain/ItemPedido.java
@@ -6,10 +6,13 @@ import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class ItemPedido implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
 	
@@ -61,7 +64,8 @@ public class ItemPedido implements Serializable{
 	public void setPreco(Double preco) {
 		this.preco = preco;
 	}
-
+	
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
