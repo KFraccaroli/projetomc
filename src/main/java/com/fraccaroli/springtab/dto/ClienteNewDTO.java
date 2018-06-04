4985575240294f51c4b0aa25857d4abd788fbfb2
Kaue Fraccaroli <kaue_fraccaroli@yahoo.com.br>
2018-06-03 23:04:41 -0300

    ajustando delete de cliente

--- src/main/java/com/fraccaroli/springtab/dto/ClienteNewDTO.java
+++ src/main/java/com/fraccaroli/springtab/dto/ClienteNewDTO.java
@@ -2,22 +2,47 @@ package com.fraccaroli.springtab.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.fraccaroli.springtab.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message = "Prenchimento obrigatório")
+	@Length(min=5, max = 120, message="O Tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message = "Prenchimento obrigatório")
+	@Email(message = "E-mail inválido")
 	private String email;
+	
+	@NotEmpty(message = "Prenchimento obrigatório")
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@NotEmpty(message = "Prenchimento obrigatório")
 	private String logradouro;
+	
+	@NotEmpty(message = "Prenchimento obrigatório")
 	private String numero;
+	
 	private String complemento;
+	
 	private String bairro;
+	
+	@NotEmpty(message = "Prenchimento obrigatório")
 	private String cep;
 	
+	@NotEmpty(message = "Prenchimento obrigatório")
 	private String telefone1;
+	
 	private String telefone2;
+	
 	private String telefone3;
 	
 	private Integer cidadeId;
---
+++ src/main/java/com/fraccaroli/springtab/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,38 @@
+package com.fraccaroli.springtab.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.fraccaroli.springtab.domain.enums.TipoCliente;
+import com.fraccaroli.springtab.dto.ClienteNewDTO;
+import com.fraccaroli.springtab.resources.exception.FieldMessage;
+import com.fraccaroli.springtab.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+
+		if(objDto.getTipo().equals(TipoCliente.PESSOASFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj","CPF INVÁLIDO"));
+		}
+
+		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj","CNPJ INVÁLIDO"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
