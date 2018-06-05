9ae0309958799ce56104a456c2816c1144297b74
Kaue Fraccaroli <kaue_fraccaroli@yahoo.com.br>
2018-06-04 21:58:57 -0300

    inserindo Pedido

--- src/main/java/com/fraccaroli/springtab/domain/PagamentoComCartao.java
+++ src/main/java/com/fraccaroli/springtab/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.fraccaroli.springtab.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.fraccaroli.springtab.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 	private static final long serialVersionUID = 1L;
 	
--- src/main/java/com/fraccaroli/springtab/services/PedidoService.java
+++ src/main/java/com/fraccaroli/springtab/services/PedidoService.java
@@ -1,12 +1,19 @@
 package com.fraccaroli.springtab.services;
 
+import java.util.Date;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.fraccaroli.springtab.domain.Categoria;
+import com.fraccaroli.springtab.domain.ItemPedido;
+import com.fraccaroli.springtab.domain.PagamentoComBoleto;
 import com.fraccaroli.springtab.domain.Pedido;
+import com.fraccaroli.springtab.domain.enums.EstadoPagamento;
+import com.fraccaroli.springtab.repositories.ItemPedidoRepository;
+import com.fraccaroli.springtab.repositories.PagamentoRepository;
 import com.fraccaroli.springtab.repositories.PedidoRepository;
 import com.fraccaroli.springtab.services.exceptions.ObjectNotFoundException;
 
@@ -16,9 +23,45 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;	
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;	
+	
 	public Pedido find(Integer id){
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 					"Objeto não encontrato! id: " +  id + ", Tipo: " + Pedido.class.getName()));
 	}
+	
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		
+		for(ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.00);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		
+		return obj;
+	}	
 }
