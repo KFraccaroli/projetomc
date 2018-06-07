a4fa102ef404e07fab16e3fc7c545c8d23f1fba6
Kaue Fraccaroli <kaue_fraccaroli@yahoo.com.br>
2018-06-06 23:54:30 -0300

    Implementando SmtpEmailService com servidor google

--- src/main/java/com/fraccaroli/springtab/config/DevConfig.java
+++ src/main/java/com/fraccaroli/springtab/config/DevConfig.java
@@ -2,6 +2,8 @@ package com.fraccaroli.springtab.config;
 
 import java.text.ParseException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -9,11 +11,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.fraccaroli.springtab.services.DBService;
+import com.fraccaroli.springtab.services.EmailService;
+import com.fraccaroli.springtab.services.MockEmailService;
+import com.fraccaroli.springtab.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
 public class DevConfig {
 
+	private static final Logger LOG = LoggerFactory.getLogger(DevConfig.class);
+	
 	@Autowired
 	private DBService dbService;
 	
@@ -23,9 +30,14 @@ public class DevConfig {
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
 		if(!"create".equals(strategy)) {
-			dbService.instantiateTestDatabase();
+			return false;
 		}
+		dbService.instantiateTestDatabase();
 		return true;
 	}
 	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
 }
