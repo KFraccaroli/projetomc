8afea2bad90b9aedf6396ba34bc0630a028d8ddf
Kaue Fraccaroli <kaue_fraccaroli@yahoo.com.br>
2018-06-07 21:05:37 -0300

    Salvando perfil de usuario na base de dados

--- src/main/java/com/fraccaroli/springtab/services/DBService.java
+++ src/main/java/com/fraccaroli/springtab/services/DBService.java
@@ -20,6 +20,7 @@ import com.fraccaroli.springtab.domain.PagamentoComCartao;
 import com.fraccaroli.springtab.domain.Pedido;
 import com.fraccaroli.springtab.domain.Produto;
 import com.fraccaroli.springtab.domain.enums.EstadoPagamento;
+import com.fraccaroli.springtab.domain.enums.Perfil;
 import com.fraccaroli.springtab.domain.enums.TipoCliente;
 import com.fraccaroli.springtab.repositories.CategoriaRepository;
 import com.fraccaroli.springtab.repositories.CidadeRepository;
@@ -116,12 +117,20 @@ public class DBService {
 
 		Cliente cli1 = new Cliente(null, "Maria Silva", "kcfraccaroli@gmail.com", "36378912377", TipoCliente.PESSOASFISICA, pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Cliente cli2 = new Cliente(null, "Kaue", "kaue_fraccaroli@yahoo.com.br", "56114043081", TipoCliente.PESSOASFISICA, pe.encode("123"));
+		cli2.getTelefones().addAll(Arrays.asList("38123456", "974545555"));
+		cli2.addPerfil(Perfil.ADMIN);
+		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida floriano", "2010", null, "Centro", "38777012", cli2, c2);
+		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1,cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
