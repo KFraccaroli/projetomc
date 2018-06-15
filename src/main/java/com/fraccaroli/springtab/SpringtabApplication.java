b4c4b015145c979e168ed06235816753825c3f91
Kaue Fraccaroli <kaue_fraccaroli@yahoo.com.br>
2018-06-14 21:35:06 -0300

    Salvando primeiro arquivo no s3

--- src/main/java/com/fraccaroli/springtab/SpringtabApplication.java
+++ src/main/java/com/fraccaroli/springtab/SpringtabApplication.java
@@ -1,12 +1,18 @@
 package com.fraccaroli.springtab;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fraccaroli.springtab.services.S3Service;
+
 @SpringBootApplication
 public class SpringtabApplication implements CommandLineRunner{
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringtabApplication.class, args);
@@ -15,6 +21,7 @@ public class SpringtabApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-	
+		s3Service.uploadFile("C:\\Users\\kfrac\\OneDrive\\Documentos\\Projetos\\img\\cat1.jpg");
+		
 	}
 }
